51ed8049f1d1ba287727a8c33913b5d768f784e2
Piyush Chamoli <piyushchamoli11@gmail.com>
2024-11-19 23:58:27 +0530

    Add code for Lecture 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", { id: "header", abc: "xyz" }, [
+  "First line of react",
+  React.createElement(
+    "h2",
+    { id: "sub-header", abc: "xyz" },
+    "Child of header element"
+  ),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
