988239b661139040b5533ceb70aeea9734691d16
Piyush Chamoli <piyushchamoli11@gmail.com>
2024-11-26 19:58:50 +0530

    initial setup

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-const Heading = () => <h1 className="head">First Heading in JSX</h1>;
-
-const Main = () => (
-  <div className="container">
-    <Heading />
-    <p>lorem ipsum dolor sit amet</p>
-  </div>
-);
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Main />);
+root.render();
