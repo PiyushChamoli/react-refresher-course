90fcb1d78a5afffb76e730d47536464fe811eb97
Piyush Chamoli <piyushchamoli11@gmail.com>
2024-11-28 00:02:28 +0530

    add search

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,8 @@
 // Lecture 5
 Two types of exports and imports: One is default export default name / import name from "src" and second is named export as in export const name / import {name} from "src".
+React hooks are normal JS utility functions.
+Whenever a state variable updates, react re renders the component.
+useState goes const [name, setName] = useState([]);
+Reconciliation Algo(React Fiber): Scenario is res.container has 10 restaurants which is changed now to 7 restaurants. It happens in such a way that Diffing Algo checks the change between Virtual DOM and Actual DOM and re renders the component which has changed.
+useState has a second set function for changes as it triggers the diffing algo to work.
+React is fast because it does efficient fast DOM manipulation.
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,44 @@
 import RestaurantUI from "./RestaurantUI";
 import resList from "./Mockdata";
+import { useState } from "react";
+
+function filterRes(val, res) {
+  console.log("searchText", val);
+  console.log(res);
+  let data = res.filter((r) => r.info.name.toLowerCase().includes(val));
+  return data;
+}
 
 const Body = () => {
+  let [restaurantList, setRestaurantList] = useState(resList);
+  let [searchText, setSearchText] = useState("");
   return (
-    <div className="body flex">
-      {resList.map((restaurant) => (
-        <RestaurantUI key={restaurant.info.id} resData={restaurant} />
-      ))}
+    <div className="body">
+      <input
+        type="text"
+        placeholder="What would you like to eat!"
+        className="inp-box"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          console.log("event", e);
+          e.preventDefault();
+          let filteredRes = filterRes(searchText, restaurantList);
+          setRestaurantList(filterRes);
+          console.log("filteredResList", filteredRes);
+        }}
+      >
+        Search Restaurant
+      </button>
+      <div className="flex">
+        {restaurantList.map((restaurant) => (
+          <RestaurantUI key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
   );
 };
